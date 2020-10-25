ff5a74230e7925422d70820f5342a7a062810c10
dietterc <20259197+dietterc@users.noreply.github.com>
2020-10-24 23:56:56 -0500

    added commands skeleton

--- botmain.py
+++ botmain.py
@@ -5,11 +5,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+PREFIX = "."
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    #guild = discord.utils.get(client.guilds, name=GUILD)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content[0] == PREFIX:
+        msg = message.content[1:]
+        if msg == "form":
+            await message.channel.send("Command received!")
+        elif msg.split()[0] == "colour":
+            await message.channel.send("Command 2 received!")
+
 
 client.run(TOKEN)
+
