97a180656810a9fa422561e4e2902db5d75d808e
dietterc <20259197+dietterc@users.noreply.github.com>
2020-11-10 13:22:40 -0600

    added deleting colours, actual role not removed yet

--- botmain.py
+++ botmain.py
@@ -14,8 +14,7 @@ colourRoles = []
 
 #read in data (may change from txt file if issues come up)
 coloursFile = open("colourRoles.txt","r")
-for line in coloursFile:
-    colourRoles.append(line)
+colourRoles = coloursFile.read().split("\n")
 coloursFile.close()
 
 bot = commands.Bot(command_prefix=PREFIX)
@@ -25,6 +24,8 @@ bot = commands.Bot(command_prefix=PREFIX)
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     #guild = discord.utils.get(client.guilds, name=GUILD)
+    for x in colourRoles:
+        print(x)
 
 #default format for commands, where the function name is the command to type
 @bot.command()
@@ -37,7 +38,7 @@ async def colour(ctx, *args):
 
     def checkColours(colour):
         for i in colourRoles:
-            if(i == colour):
+            if(i is colour):
                 return False
         return True
     
@@ -50,7 +51,7 @@ async def colour(ctx, *args):
                     guild = ctx.guild
                     roleName = args[2].lower()
                     await guild.create_role(name=roleName,colour=discord.Colour(int(colour[1:], 16)))
-                    await ctx.send("Colour role: " + roleName + " added.")
+                    await ctx.send("Colour role: `" + roleName + "` added.")
 
                     #add the new colour to our file, then add it to our list
                     coloursFile = open("colourRoles.txt", "a")
@@ -62,10 +63,24 @@ async def colour(ctx, *args):
             else:
                 await ctx.send("Error: Colour role with that name already exists.")
         else: 
-            await ctx.send("Error: Correct format is: " + PREFIX + "colour add #{{colour}} {{label}}")
+            await ctx.send("Error: Correct format is: `" + PREFIX + "colour add #{{hexColour}} {{label}}`")
 
     elif(args[0] == 'delete'):
-        print()
+        #removing colours 
+        if(len(args) == 2):
+            if(checkColours(args[1].lower())):
+                #colour exits, remove it
+                colourRoles.remove(args[1].lower())
+                #rewrite the file
+                coloursFile = open("colourRoles.txt", "w")
+                for i in colourRoles:
+                    coloursFile.write("\n" + i)
+                coloursFile.close()
+                await ctx.send("Colour role `" + args[1].lower() + "` removed.")
+            else:
+                await ctx.send("Error: Colour role `" + args[1] + "` not found.")
+        else:
+            await ctx.send("Error: Correct format is: `" + PREFIX + "colour delete {{colour}}`")
     
     else:
         print()
