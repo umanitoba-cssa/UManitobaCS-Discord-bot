6237e06a11505840905b277f28464413e75d7cbe
dietterc <20259197+dietterc@users.noreply.github.com>
2020-11-10 00:40:08 -0600

    added a way to store colour roles

--- botmain.py
+++ botmain.py
@@ -9,8 +9,15 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 PREFIX = '.'
 
+#lists
+colourRoles = []
 
-#client = discord.Client()
+#read in data (may change from txt file if issues come up)
+coloursFile = open("colourRoles.txt","r")
+for line in coloursFile:
+    colourRoles.append(line)
+coloursFile.close()
+
 bot = commands.Bot(command_prefix=PREFIX)
 
 
@@ -27,21 +34,35 @@ async def test(ctx, *args):
 
 @bot.command()
 async def colour(ctx, *args):
+
+    def checkColours(colour):
+        for i in colourRoles:
+            if(i == colour):
+                return False
+        return True
     
     if(args[0] == 'add'):
         # adding colours
         if(len(args) == 3):
             colour = args[1]
-            if(colour[0] == '#' and len(colour) == 7):
-                guild = ctx.guild
-                roleName = "c:" + args[2]
-                await guild.create_role(name=roleName,colour=discord.Colour(int(colour[1:], 16)))
-                await ctx.send("Colour role: " + roleName + " added.")
+            if(checkColours(args[2].lower())):
+                if(colour[0] == '#' and len(colour) == 7):
+                    guild = ctx.guild
+                    roleName = args[2].lower()
+                    await guild.create_role(name=roleName,colour=discord.Colour(int(colour[1:], 16)))
+                    await ctx.send("Colour role: " + roleName + " added.")
+
+                    #add the new colour to our file, then add it to our list
+                    coloursFile = open("colourRoles.txt", "a")
+                    coloursFile.write("\n" + roleName)
+                    coloursFile.close()
+                    colourRoles.append(roleName)
+                else:
+                    await ctx.send("Error: Invalid hex input: " + colour)
             else:
-                await ctx.send("Error: Invalid hex input: " + colour)
+                await ctx.send("Error: Colour role with that name already exists.")
         else: 
             await ctx.send("Error: Correct format is: " + PREFIX + "colour add #{{colour}} {{label}}")
-
 
     elif(args[0] == 'delete'):
         print()
